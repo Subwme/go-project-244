3c65c112880c0afa17ff007a09f1ce638819d3ef
urman <aleksandr.u@livetex.ru>
2026-05-20 01:17:15 +0200

    Fix JSON formatter to output object instead of array

--- internal/formatter/json.go
+++ internal/formatter/json.go
@@ -7,9 +7,45 @@ import (
 )
 
 func JSON(nodes []diff.Node) (string, error) {
-	data, err := json.MarshalIndent(nodes, "", "    ")
+	result := nodesToMap(nodes)
+	data, err := json.MarshalIndent(result, "", "    ")
 	if err != nil {
 		return "", err
 	}
 	return string(data), nil
+}
+
+func nodesToMap(nodes []diff.Node) map[string]interface{} {
+	result := make(map[string]interface{})
+	for _, node := range nodes {
+		switch node.Type {
+		case diff.Nested:
+			result[node.Key] = map[string]interface{}{
+				"type":     "nested",
+				"children": nodesToMap(node.Children),
+			}
+		case diff.Added:
+			result[node.Key] = map[string]interface{}{
+				"type":  "added",
+				"value": node.NewValue,
+			}
+		case diff.Removed:
+			result[node.Key] = map[string]interface{}{
+				"type":  "removed",
+				"value": node.OldValue,
+			}
+		case diff.Unchanged:
+			result[node.Key] = map[string]interface{}{
+				"type":  "unchanged",
+				"value": node.Value,
+			}
+		case diff.Changed:
+			result[node.Key] = map[string]interface{}{
+				"type": "changed",
+				"old":  node.OldValue,
+				"new":  node.NewValue,
+			}
+		}
+	}
+	return result
 }
